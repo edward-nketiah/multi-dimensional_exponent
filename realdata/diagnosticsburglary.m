6e6f5ffeda609f04eeb1fe0971074450d17ba34c
eddy22 <72153099+edward-nketiah@users.noreply.github.com>
2026-02-04 21:32:42 +0800

    Add files via upload

---
+++ realdata/diagnosticsburglary.m
@@ -0,0 +1,139 @@
+clear
+clc
+%rng(100)%0110
+% Generate artificial data
+data2 = load('Burglary2002.mat');
+data3 = data2.data1;
+
+lat = data3(:,1);
+
+lon = data3(:,2);
+
+% Convert latitude and longitude to kilometers
+% Using a reference latitude (e.g., the mean latitude) for accurate scaling
+reference_lat = mean(lat);
+lat_km = (lat - reference_lat) * 111; % Latitude scale is roughly 111 km per degree
+lon_km = (lon - mean(lon)) * 111 * cosd(reference_lat); % Adjust longitude for latitude
+
+% Shift coordinates to start from zero
+lat_km = lat_km - min(lat_km);
+lon_km = lon_km - min(lon_km);
+
+
+x1 = lon_km;
+y1 = lat_km;
+
+%k = zeros(size(x1));
+S = [15 23 15 30];
+t1 = data3(:,3);
+
+%t1 = t1-t1(1);%Time Shift
+n = length(x1);
+n1 = 200;%The most recent 200 points are used for calculation
+X = zeros(n1-1,n-1);%This value indicates that the n1 data points closest to the time point are used to calculate g in lambda
+Y = X;
+T = X;
+%eps =1e-4;
+for j = 1:n1-1
+    X(j,:) = x1(2:end)-[-Inf*ones(j-1,1);x1(1:end-j)];
+    Y(j,:) = y1(2:end)-[-Inf*ones(j-1,1);y1(1:end-j)];
+    T(j,:) = t1(2:end)-[-Inf*ones(j-1,1);t1(1:end-j)];
+end
+
+% BIC_5 = load('BICN.mat');% felarrest
+% BIC_5 = BIC_5.BIC;
+
+BIC_5 = load('BICBURG2002.mat'); % felony crime
+BIC_5 = BIC_5.BIC;
+rng(115)
+dist = 0:0.01:3;
+% L1 = zeros(19,length(dist));
+L = zeros(3,length(dist));
+L1 = zeros(3,length(dist));
+x = BIC_5{1,2};
+p =0+1;
+q = 1+1;
+
+for i = 1:10
+ [data,~,k] = di_method(x,x1,y1,t1,X,Y,T,S,p,q);
+ n = poissrnd(k*(S(2)-S(1))*(S(4)-S(3))*364.9); %length(data3)
+  if n>0
+    rand_vals = rand(n,3);
+    space_time_data0 = [rand_vals(:,1)*(S(2)-S(1))+S(1),rand_vals(:,2)*(S(4)-S(3))+ S(3),rand_vals(:,3)*364.9];
+    x10 = space_time_data0(:,1);%Space-time coordinates
+    y10 = space_time_data0(:,2);
+    t10 = space_time_data0(:,3);
+    t10 = t10-t10(1);%Time Shift
+    n0 = length(x10);
+    n10 = min(200,n0);%The most recent 200 points are used for calculation
+    X0 = zeros(n10-1,n0-1);%This value indicates that the n1 data points closest to the time point are used to calculate g in lambda.
+    Y0 = X0;
+    T0 = X0;
+    for j = 1:n10-1
+      X0(j,:) = x10(2:end)-[-Inf*ones(j-1,1);x10(1:end-j)];
+      Y0(j,:) = y10(2:end)-[-Inf*ones(j-1,1);y10(1:end-j)];
+      T0(j,:) = t10(2:end)-[-Inf*ones(j-1,1);t10(1:end-j)];
+    end
+    [~,lambda,~] = di_method(x,x10,y10,t10,X0,Y0,T0,S,p,q);   
+    
+    %temp = (k)./lambda>rand(1,n);
+    temp = min((k./lambda),1) >rand(1,n);
+    % using the super thin simulate inhomogeneous Poisson process with rate
+    % max(k-lambda,0)
+    super_thin_rate = max((k-lambda),0);
+    n_super = poissrnd(sum(super_thin_rate)*(S(2)-S(1))*(S(4)-S(3))*364.9);
+    sup_rand_vals = rand(n_super,3);
+    super_thin_data = [sup_rand_vals(:,1)*(S(2)-S(1))+S(1),sup_rand_vals(:,2)*(S(4)-S(3))+S(3),sup_rand_vals(:,3)*364.9];
+
+    %combine the two process
+    data = [space_time_data0(temp,:);super_thin_data];
+  end 
+    
+  
+ K = RipleysK(data(:,1:2),dist,S);
+ N = poissrnd(size(data,1));
+ x0 = [rand(N,1)*(S(2)-S(1))+S(1),rand(N,1)*(S(4)-S(3))+S(3)];
+ K1 = RipleysK(x0,dist,S);
+ L(i,:) = sqrt(K/pi)-dist';
+ L1(i,:) = sqrt(K1/pi)-dist';
+end
+%subplot(1,2,1)
+%plot(data(:,1),data(:,2),'.')
+%subplot(1,2,2)
+%plot(x(:,1),x(:,2),'.')
+% plot(data(:,1),data(:,2),'.')
+% claculating  the 95% confidence bound for both L and L1
+% Calculate the 95% confidence bounds for both L and L1
+%lower_bound_L = prctile(L, 2.5, 1);   % 5% lower bound
+%upper_bound_L = prctile(L, 97.5, 1);  % 95% upper bound
+
+%lower_bound_L1 = prctile(L1, 2.5, 1);   % 5% lower bound for homogeneous Poisson process
+%upper_bound_L1 = prctile(L1, 97.5, 1);  % 95% upper bound for homogeneous Poisson process
+
+% Mean values of L-functions
+%mean_L = mean(L, 1);
+%mean_L1 = mean(L1, 1);
+
+%std_L = std(L);
+%n = size(L,1);
+%SE_L = std_L/sqrt(n);
+
+%upper_bound = mean_L + 2*SE_L;
+%lower_bound = mean_L - 2*SE_L;
+% Plot the L-functions and their confidence intervals
+figure(1)
+%subplot(1, 2, 1)
+
+plot(dist,min(L),'b-')
+hold on 
+plot(dist,max(L),'b-')
+hold on
+shadedplot(dist,min(L),max(L),'c');
+grid on
+hold on
+plot(dist,mean(L),'k-')
+%plot(dist,upper_bound_L,'r-')
+%hold on
+plot(dist,[min(L1);max(L1)],'r-')
+hold off
+
---
+++ realdata/plotrealburg.m
@@ -0,0 +1,102 @@
+clearvars
+clc
+%rng 4567
+BIC_5 = load('BICBURG2002.mat');
+
+params = BIC_5.BIC{1,2};
+
+% extrating parameter for mu
+g_par = reshape(params(6:13),[4,2]);
+
+% extrating parameter for mu
+mu_par = params(1:5); % q=1
+%num_mode = 3;
+
+
+mu_b0 = mu_par(1);
+mu_b1 = mu_par(2);
+mu_b2 = mu_par(3);
+mu_b3 = mu_par(4);
+mu_b4 = mu_par(5);
+
+g_b0 = g_par(1,:);
+g_b1 = g_par(2,:);
+g_b2 = g_par(3,:);
+g_b3 = g_par(4,:);
+
+
+
+% mu_b1 = mu_b1 + 0.1;%*rand(size(mu_b1));
+% mu_b3 = mu_b3 + 0.1;%*rand(size(mu_b3));
+% mu_b2 = mu_b2*0.1;
+% mu_b4 = mu_b4*0.1;
+
+% disp(g_params);
+mu0 = @(b0,b1,b2,b3,b4,u,v)b0*exp(-b2*(u-b1).^2-b4*(v-b3).^2);
+mu_cor = -2:0.01:40;
+mu1_cor = -8:0.01:40;
+nor_mu = 0;
+
+mu_x = mu0(mu_b0,mu_b1,mu_b2,mu_b3,mu_b4,mu_cor,mu_b3)*(pi/mu_b4)^0.5;
+mu_y = mu0(mu_b0,mu_b1,mu_b2,mu_b3,mu_b4,mu_b1,mu1_cor)*(pi/mu_b2)^0.5;
+nor_mu = nor_mu + pi*mu_b0/(mu_b2*mu_b4)^0.5;
+
+nor_g = 0;
+g0 = @(b0,b1,b2,b3,u,v,s)b0*exp(-b1*u.^2-b2*v.^2-b3*s);
+g_cor = 0:0.1:20;
+g1_cor = -1:0.02:1;
+g2_cor = -4:0.1:4;
+n = length(g_cor);
+n1 = length(g1_cor);
+n2 = length(g2_cor);
+g_x = zeros(2,n1);
+g_y = zeros(2,n2);
+g_t = zeros(2,n);
+for i=1:2
+g_x(i,:) = g0(g_b0(i),g_b1(i),g_b2(i),g_b3(i),g1_cor,0,0)*(pi/g_b2(i))^0.5/g_b3(i);
+g_y(i,:) = g0(g_b0(i),g_b1(i),g_b2(i),g_b3(i),0,g2_cor,0)*(pi/g_b1(i))^0.5/g_b3(i);
+g_t(i,:) = g0(g_b0(i),g_b1(i),g_b2(i),g_b3(i),0,0,g_cor)*pi/(g_b1(i)*g_b2(i))^0.5;
+nor_g =  nor_g + pi*g_b0(i)/(g_b1(i)*g_b2(i))^0.5/g_b3(i);
+end
+
+
+figure(1)
+subplot(2,3,1)
+plot(mu_cor,sum(mu_x,1)/nor_mu)
+xticks(-2:10:40)
+
+subplot(2,3,2)
+plot(mu1_cor,sum(mu_y,1)/nor_mu)
+xticks(-20:10:40)
+%subplot(2,3,3)
+%plot(mu1_cor,sum(mu_t,1)/nor_mu)
+subplot(2,3,4)
+semilogy(g1_cor,sum(g_x,1)/nor_g)
+hold on
+ylims = ylim; %[min(sum(g_x,1)/nor_g),max(sum(g_x,1)/nor_g)];
+line([-0.55, -0.55],ylims,'color','r', 'Linestyle','--');
+line([0.55, 0.55],ylims,'color','r', 'Linestyle','--');
+text(-0.55,mean(ylims),'-0.55','Fontsize',10,'HorizontalAlignment', 'right');
+text(0.55,mean(ylims),'0.55','Fontsize',10);
+xticks(-1:0.4:1);
+xlabel('Km')
+hold off
+%xlim([-6,6]);
+%semilogy(x,y);
+subplot(2,3,5)
+semilogy(g2_cor,sum(g_y,1)/nor_g)
+hold on
+ylim = [min(sum(g_y,1)/nor_g),max(sum(g_y,1)/nor_g)];
+line([-1.8, -1.8],ylim,'color','r', 'Linestyle','--');
+line([1.8, 1.8],ylim,'color','r', 'Linestyle','--');
+text(-1.8,mean(ylim),'-1.8','Fontsize',10,'HorizontalAlignment', 'right');
+text(1.8,mean(ylim),'1.8','Fontsize',10);
+xticks(-5:1:5);
+xlabel('Km')
+hold off
+%xlim([-6,6]);
+%semilogy(x,y);
+subplot(2,3,6)
+plot(g_cor,sum(g_t,1)/nor_g)
+xticks(0:4:20);
+xlabel('Time(days)')
